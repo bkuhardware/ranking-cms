bdb0deae872fa027b80d7bcf7c28c11508c1bd59
bkuhardware <bkuhardware@linecorp.com>
2020-11-19 20:48:28 +0900

    [FE] Add dayjs

--- frontend/src/components/dashboard/DashboardTournament.tsx
+++ frontend/src/components/dashboard/DashboardTournament.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Card, CardText, CardTitle, Col } from 'reactstrap';
 import TournamentModel from "../../models/tournamentModel";
+import {formatDate} from "../../helpers/dateHelper";
 
 function DashboardTournament(props: any) {
     const tournament: TournamentModel = props.tournament;
-    const lastUpdatedStr: string = tournament.lastUpdated.toISOString();
+    const lastUpdatedStr: string = formatDate(tournament.lastUpdated);
     return (
         <Card body outline color="info" className="mb-3 tournament cursor-pointer">
             <CardTitle tag="h3">
@@ -12,7 +13,7 @@ function DashboardTournament(props: any) {
             </CardTitle>
             <CardText>{tournament.description}</CardText>
             <CardText>
-                <small className="text-muted">{lastUpdatedStr}</small>
+                <small className="text-muted">Last updated: {lastUpdatedStr}</small>
             </CardText>
         </Card>
     )
