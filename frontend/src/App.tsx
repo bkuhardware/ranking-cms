10689210b83e892415f42f127e06bcca6fe52983
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-11-21 06:46:24 +0700

    [FE] Layout of app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Router from "./Router";
 import Footer from "./components/common/Footer";
 import Navigator from "./components/common/Navigator";
+import {Container} from "reactstrap/es";
 
 const App = () => {
     return (
         <div id="app">
             <Navigator />
             <div className="app-content">
-                <main><Router /></main>
+                <Container className="main-layout">
+                    <Router />
+                </Container>
                 <Footer />
             </div>
         </div>
